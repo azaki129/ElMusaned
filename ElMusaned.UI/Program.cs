996cdfbccfc71f5fe1fedb9b92d34ae7af338471
ahmed zaki <a.zaki129@gmail.com>
2020-10-22 21:42:55 +0200

    sss

--- ElMusaned.UI/Program.cs
+++ ElMusaned.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElMusaned.UI.View.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace ElMusaned.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
